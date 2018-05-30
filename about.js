2d8726a3ef553f0cb39e262d4af5176ae4c49487
celinechang97 <35846520+celinechang97@users.noreply.github.com>
2018-05-30 16:02:04 -0500

    added everything thus far

---
+++ about.js
@@ -0,0 +1,29 @@
+function openCarousel(){
+	document.querySelector(".carousel_container").style.display="block";
+}
+
+function closeModal(){
+	document.getElementById("myModal").style.display="none"
+}
+
+
+// Instantiate the Bootstrap carousel
+$('.multi-item-carousel').carousel({
+  interval: false
+});
+
+// for every slide in carousel, copy the next slide's item in the slide.
+// Do the same for the next, next item.
+$('.multi-item-carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  if (next.next().length>0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+  } else {
+    $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+  }
+});
---
+++ what.js
@@ -0,0 +1,7 @@
+$(function() {
+  $('#scroll').on('click', function(e) {
+  	console.log("hello")
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
